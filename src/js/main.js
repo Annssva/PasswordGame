2defbd8ca2c56adebed8846edaab710ca63b7015
annssva <curlyannas0@gmail.com>
2023-08-16 17:15:51 +0300

    added dark theme

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 import '../css/style.css'
+import { darkModeHandle } from "./utils.js";
 import { startGame } from "./game.js";
+
+darkModeHandle();
 
 const textarea = document.getElementById("textAreaForPassword");
 textarea.addEventListener("input", startGame);
