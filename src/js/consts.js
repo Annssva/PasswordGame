194485d54ffe2042b7916b1945e8d3b3eed98c40
annssva <curlyannas0@gmail.com>
2023-08-17 17:08:44 +0300

    added constant to check roman numerals

--- src/js/consts.js
+++ src/js/consts.js
@@ -1,3 +1,4 @@
 export const specialCharacters = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/;
 export const months = ['january', 'february', 'march', 'april', 'may', 'june',
     'july', 'august', 'september', 'october', 'november', 'december']
+export const romanNumerals = /[IVXLCDM]/
