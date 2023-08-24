237e46108e339d0e5f901ea1438ff5b342ffead0
annssva <curlyannas0@gmail.com>
2023-08-24 18:30:31 +0300

    added favicons for dark and light theme

--- src/js/utils.js
+++ src/js/utils.js
@@ -8,6 +8,11 @@ export const darkModeHandle = () => {
     darkModeSwitcher.checked = true;
     root.style.setProperty('--scrollbar-track-bg', 'rgb(39 39 42)');
     root.style.setProperty('--scrollbar-thumb-bg', '#888');
+    const links = document.querySelectorAll('link[rel="apple-touch-icon"], link[rel="icon"]');
+    links.forEach(function(link) {
+      const newHref = link.getAttribute('href').replace('public/favicon/light/', 'public/favicon/dark/');
+      link.setAttribute('href', newHref);
+    });
   }
   // при нажатии на свитчер, меняется тема и сохраняется значение соответствующего режима в localStorage
   darkModeSwitcher.addEventListener("input", () => {
@@ -15,10 +20,20 @@ export const darkModeHandle = () => {
 
     if (htmlElement.classList.contains("dark")) {
       localStorage.setItem("mode", "dark");
+      const links = document.querySelectorAll('link[rel="apple-touch-icon"], link[rel="icon"]');
+      links.forEach(function(link) {
+        const newHref = link.getAttribute('href').replace('public/favicon/light/', 'public/favicon/dark/');
+        link.setAttribute('href', newHref);
+      });
       root.style.setProperty('--scrollbar-track-bg', 'rgb(39 39 42)');
       root.style.setProperty('--scrollbar-thumb-bg', '#888');
     } else {
       localStorage.setItem("mode", "light");
+      const links = document.querySelectorAll('link[rel="apple-touch-icon"], link[rel="icon"]');
+      links.forEach(function(link) {
+        const newHref = link.getAttribute('href').replace('public/favicon/dark/', 'public/favicon/light/');
+        link.setAttribute('href', newHref);
+      });
       root.style.setProperty('--scrollbar-track-bg', 'rgb(224 231 255)');
       root.style.setProperty('--scrollbar-thumb-bg', 'rgb(165 180 252)');
     }
